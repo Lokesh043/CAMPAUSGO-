4512efbb5de795b41939181b59f4c9d38c8107a4
Rachamalla Lokesh <159107854+Lokesh043@users.noreply.github.com>
2025-11-18 23:59:15 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        sruBlue: "#004AAD",
+        sruDark: "#002D73"
+      }
+    }
+  },
+  plugins: []
+}
